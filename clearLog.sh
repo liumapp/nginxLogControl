2d37ee5c74517d0d3314023f39f0894deda027f8
liumapp <liumapp.com@gmail.com>
2017-06-01 15:21:06 +0800

    adjust code by mv

--- clearLog.sh
+++ clearLog.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cp /usr/local/log/error.log /usr/local/log/error-$(date +"%Y%m%d").log
+mv /usr/local/log/error.log /usr/local/log/error-$(date +"%Y%m%d").log
 
-cat /dev/null >> /usr/local/log/error.log
+#cat /dev/null > /usr/local/log/error.log
 
-cp /usr/local/log/access.log /usr/local/log/access-$(date +"%Y%m%d").log
+mv /usr/local/log/access.log /usr/local/log/access-$(date +"%Y%m%d").log
 
-cat /dev/null >> /usr/local/log/access.log
+#cat /dev/null > /usr/local/log/access.log
